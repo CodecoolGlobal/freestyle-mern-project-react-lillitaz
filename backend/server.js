56183667dab2626d674ec7867b305c41786139d5
San <san.kuno22@gmail.com>
2023-03-09 11:58:50 +0100

    changed the MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-import express  from "express";
+import express from "express";
 import User from "./model/User.js";
-import dotenv from 'dotenv';
-import cors from 'cors';
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,88 +11,89 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, ()=> console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`));
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-  })
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); 
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  useUnifiedTopology: true,
 });
 
-app.post('/api/create', (req, res) => {
-  const email = req.body.email
-  const userName = req.body.userName
-  const password = req.body.password
+app.post("/api/create", (req, res) => {
+  const email = req.body.email;
+  const userName = req.body.userName;
+  const password = req.body.password;
 
   const user = new User({
     email,
     userName,
     password,
-    
   });
   console.log(user);
-  user.save()
-    .then(user => res.json(user))
-    .catch(err => {
+  user
+    .save()
+    .then((user) => res.json(user))
+    .catch((err) => {
       console.log(err);
       res.status(400).json({ success: false });
-    })
-});
-
-
-app.post('/api/login', (req, res) => {
-  const { userName, password } = req.body;
-  
-  User.findOne({ userName, password })
-    .then(user => {
-      if (user) {
-        res.json({ success: true });
-      } else {
-        res.status(401).json({ success: false, message: 'Invalid username or password' });
-      }
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({ success: false, message: 'Internal server error' });
     });
 });
 
-app.post('/api/favorites', requireLogin, (req, res) => {
+app.post("/api/login", (req, res) => {
+  const { userName, password } = req.body;
+
+  User.findOne({ userName, password })
+    .then((user) => {
+      if (user) {
+        res.json({ success: true });
+      } else {
+        res
+          .status(401)
+          .json({ success: false, message: "Invalid username or password" });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
+    });
+});
+
+app.post("/api/favorites", requireLogin, (req, res) => {
   const userName = req.body.currentUser;
-  console.log(req.body)
-  console.log(userName)
+  console.log(req.body);
+  console.log(userName);
 
   User.findOne({ userName })
-    .then(user => {
+    .then((user) => {
       if (!user) {
-        res.status(401).json({ success: false, message: 'User not found' });
+        res.status(401).json({ success: false, message: "User not found" });
       } else {
         user.favorites.push({
           title: req.body.title,
           year: req.body.year,
           poster: req.body.poster,
-          imdbId: req.body.imdbId
+          imdbId: req.body.imdbId,
         });
-        user.save()
+        user.save();
         res.status(200).json({ success: true, favorites: user.favorites });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
-      res.status(500).json({ success: false, message: 'Could not save' });
+      res.status(500).json({ success: false, message: "Could not save" });
     });
-  })
+});
 
 function requireLogin(req, res, next) {
-if (!req.body.currentUser) {
-  res.status(401).json({ success: false, message: 'You must be logged in to perform this action' });
-} else {
-  next();
-}
+  if (!req.body.currentUser) {
+    res
+      .status(401)
+      .json({
+        success: false,
+        message: "You must be logged in to perform this action",
+      });
+  } else {
+    next();
+  }
 }
